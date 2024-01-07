7e4e7fda15bc768b7282ffda1aa30335ad1818a0
Maximilian Vieres <84975868+mvieres@users.noreply.github.com>
2024-01-07 14:57:20 +0100

    Transfer

--- checkComment.m
+++ checkComment.m
@@ -1,27 +1,30 @@
-function out = checkComment(str)
+function out = checkComment(cell_array)
     % SYNTAX: out = checkComment(str)
     %
     % Checks for Ifdiff comments to handly nodes differently
     % Input: String (comment from node)
-    % 
+    % Input: Cell Array of form 1x variable length
     % Output: True / false
     %
-    % Anmerkung: Ist sehr langsam
+    % Anmerkung: Ist sehr langsam, geht nicht wenn IFDIFF : oder aehnlich
     %
-
-    out = false;
-    if contains(str, 'IFDIFF:') 
-        % TODO
-        if contains(str, 'ignore')
-            out = true;
-        elseif contains(str, 'equals')
-            out = true;
-        elseif contains(str, 'filippov')
-            out = true;
+    len_cell = length(cell_array);
+    out = zeros(1, len_cell);
+    for i = 1:len_cell
+        str = cell_array{i};
+        if contains(str, '% IFDIFF:') 
+            if contains(str, 'ignore')
+                out(i) = 1;
+            elseif contains(str, 'equals')
+                out(i) = 2;
+            elseif contains(str, 'filippov')
+                out(i) = 3;
+            else
+                out(i) = -2;
+                return
+            end
         else
-            return
+            out(i) = -1;
         end
-    else
-        return
     end
 end
--- rhs_test.m
+++ rhs_test.m
@@ -1,13 +1,13 @@
 function dx = rhs_test(t,x,p)
     
     if x(1) > p(1) 
-        dx(1) = 0;
+        dx(1) = 0; % Testkommentar 1
     else
         dx(1) = t;
-        if x(2) > p(1) % IFDIFF:ignore
+        if x(2) > p(1) 
             dx(1) = 0; 
         end
-
+        % Testkommentar 2
         if x(1) < p(2) 
             dx(1) = 10;
         end
