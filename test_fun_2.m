91af594b921f436560807d92969fb8b5613af4db
Maximilian Vieres <84975868+mvieres@users.noreply.github.com>
2024-01-17 17:37:16 +0100

    Test Funktionen für IFDIFF ignore

---
+++ test_fun_2.m
@@ -0,0 +1,20 @@
+function dx = test_fun_2(t,x,p)
+
+
+dx = zeros(2,1);
+dx(1) = 0.01 * t.^2  +  x(2).^3;
+
+if x(1) > p(1)
+    dx(1) = 3;
+else
+    dx(1) = 0;
+end
+
+
+if x(2) > p(2) % IFDIFF:ignore
+    dx(2) = 100;
+else
+    dx(2) = t.^2;
+end
+
+end
