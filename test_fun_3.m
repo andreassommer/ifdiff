8056b0b72db6018ee2679ba9ffe7c3dd8f6c4d89
Maximilian Vieres <84975868+mvieres@users.noreply.github.com>
2024-01-17 18:05:42 +0100

    Update test_fun_3.m

--- test_fun_3.m
+++ test_fun_3.m
@@ -1,23 +1,20 @@
 function dx = test_fun_3(t,x,p)
 
-
-if any(isnan(x))
-    warning('Achtung')
-end
-
-
 dx = zeros(2,1);
 dx(1) = 0.01 * t.^2  +  x(2).^3;
+if ~any(isnan(x))
+    if x(1) > p(1) % IFDIFF:ignore
+        dx(1) = 3;
+    else
+        dx(1) = 0;
+    end
 
-if x(1) > p(1)
-    dx(1) = 3;
+    warning('Achtung')
 else
-    dx(1) = 0;
+    dx(1) = 10;
 end
 
-
-
-if x(2) > p(2) % IFDIFF:ignore
+if x(2) > p(2) 
     dx(2) = 100;
 else
     dx(2) = t.^2;
