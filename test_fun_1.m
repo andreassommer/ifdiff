11c9d68eb9c2db52cfb52295729d887fa5370ad4
Maximilian Vieres <84975868+mvieres@users.noreply.github.com>
2024-01-20 08:36:39 +0100

    Test function for IFDIFF ignore

--- test_fun_1.m
+++ test_fun_1.m
@@ -6,12 +6,14 @@ dx(1) = 0.01 * t.^2  +  x(2).^3;
 if x(1) < p(1) 
     dx(2) = 0;
 else
-    if x(1) < p(1) + 0.5 % IFDIFF:ignore
-        dx(2) = 5;
+    %IFDIFF: ignore
+    if x(1) < p(1) + 0.5 
+        dx(2) = 5; % Testkommentar 2
         
-    else
+    else % Testkommentar 3
         dx(2) = 0;
     end
+    
 end
 
 
--- test_fun_3.m
+++ test_fun_3.m
@@ -3,7 +3,7 @@ function dx = test_fun_3(t,x,p)
 dx = zeros(2,1);
 dx(1) = 0.01 * t.^2  +  x(2).^3;
 if ~any(isnan(x))
-    if x(1) > p(1) % IFDIFF:ignore
+    if x(1) > p(1) 
         dx(1) = 3;
     else
         dx(1) = 0;
@@ -14,7 +14,7 @@ else
     dx(1) = 10;
 end
 
-if x(2) > p(2) 
+if (x(2) > p(2)) % Klammern funktionieren nicht --> Fatal error 
     dx(2) = 100;
 else
     dx(2) = t.^2;
