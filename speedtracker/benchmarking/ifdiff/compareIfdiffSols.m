b14162e57bfe2d01c1abf1d7b3baee2f6b514e4e
Kilian Folger <kilulafo@gmail.com>
2024-04-20 15:28:59 +0200

    chore: remove use of ismembertol (it is from R2015)

--- speedtracker/benchmarking/ifdiff/compareIfdiffSols.m
+++ speedtracker/benchmarking/ifdiff/compareIfdiffSols.m
@@ -2,13 +2,15 @@ function areEqual = compareIfdiffSols(sol1, sol2)
 %COMPAREIFDIFFSOLS Compare two augmented sol objects from IFDIFF integration on their final y values and switches
     relTol = ConfigProvider.getUserConfig().YEndTol();
     areEqual = true;
-    if ~all(all(ismembertol(sol1.y(:, end), sol2.y(:, end), relTol)))
+    y1End = sol1.y(:, end);
+    y2End = sol2.y(:, end);
+    if ~all(abs(y1End - y2End) <= relTol * max(abs([y1End(:); y2End(:)])), 'all')
         areEqual = false;
     end
     if (size(sol1.switches) ~= size(sol2.switches))
         areEqual = false;
     end
-    if ~all(all(ismembertol(sol1.switches, sol2.switches, relTol)))
+    if ~all(abs(sol1.switches - sol2.switches) <= relTol * max(abs([sol1.switches(:); sol2.switches(:)])), 'all')
         areEqual = false;
     end
 end
