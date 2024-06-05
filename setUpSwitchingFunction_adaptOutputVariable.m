3543b102bee6b37421e8c92b5b2cf2a2c05a169f
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-06-05 15:07:36 +0200

    refactor(setUpSwitchingFunction): small renames, comments, spacing

--- setUpSwitchingFunction_adaptOutputVariable.m
+++ setUpSwitchingFunction_adaptOutputVariable.m
@@ -1,32 +1,30 @@
-function switchingFcn = setUpSwitchingFunction_adaptOutputVariable(switchingFcn, function_index, j, n)
+function [switchingFcn, fcnCallIndex] = setUpSwitchingFunction_adaptOutputVariable(switchingFcn, i, j, n)
 % when there is:
 % y = preprocessed_fun(function_index, datahandle, y);
 % replace it by
 % returnvalue = preprocessed_fun(y);
-%
 % and change output variable in the function header to returnvalue
 
-config = makeConfig(); 
-cIndex = mtree_cIndex(); 
+config = makeConfig();
+cIndex = mtree_cIndex();
 
 rIndex = struct('HEAD', struct(), 'BODY', struct());
 rIndex.HEAD = mtree_rIndex_head(switchingFcn.mtreeobj_switchingFcn{3,n}, rIndex.HEAD);
-function_index_i = switchingFcn.function_index_t1{function_index};
-u = (function_index_i(j) == switchingFcn.mtreeobj_switchingFcn{4,n});
+function_index_i = switchingFcn.function_index_t1{i};
+function_index_j = function_index_i(j);
+u = (function_index_j == switchingFcn.mtreeobj_switchingFcn{4,n});
 rIndex_fcn = switchingFcn.mtreeobj_switchingFcn{5,n};
 
 % change the name of the function call that contains the switch
 [switchingFcn.mtreeobj_switchingFcn{3,n}, ~] = mtree_createAndAdd_NewNode(switchingFcn.mtreeobj_switchingFcn{3,n}, ...
     rIndex_fcn.Call(u), ...             % from
     cIndex.indexLeftchild, ...                % from_type
-    {switchingFcn.mtreeobj_switchingFcn{3,n}.K.ID, switchingFcn.name_new}); 
+    {switchingFcn.mtreeobj_switchingFcn{3,n}.K.ID, switchingFcn.name_new});
 
 [switchingFcn.mtreeobj_switchingFcn{3,n}, ~] = mtree_createAndAdd_NewNode(switchingFcn.mtreeobj_switchingFcn{3,n}, ...
     rIndex_fcn.Equals(u), ...                               % from
     cIndex.indexLeftchild, ...                % from_type
     {switchingFcn.mtreeobj_switchingFcn{3,n}.K.ID, config.switchingfunction.name_outputvariable});
-
-
 
 % new Output for function in mtree_switchingFcn
 [switchingFcn.mtreeobj_switchingFcn{3,n}, ~] = mtree_createAndAdd_NewNode(switchingFcn.mtreeobj_switchingFcn{3,n}, ...
@@ -34,9 +32,5 @@ rIndex_fcn = switchingFcn.mtreeobj_switchingFcn{5,n};
     cIndex.indexLeftchild, ...                % from_type
     {switchingFcn.mtreeobj_switchingFcn{3,n}.K.ID,config.switchingfunction.name_outputvariable}); 
 
-
-
-switchingFcn.u = u; 
-
-
+fcnCallIndex = u; 
 end
--- setUpSwitchingFunction_setUpFcnCall.m
+++ setUpSwitchingFunction_setUpFcnCall.m
@@ -1,20 +1,11 @@
-function switchingFcn = setUpSwitchingFunction_setUpFcnCall(switchingFcn, i, j) 
-% consider function call with index j 
-% get the correpesponding function, change its name and adapt 
-% the function call, i.e. replace old name of fcn with new one. 
-% create a new mtreeplus object with new function if not existing
-% if it exist, use the existing 
-% 
-% i is row index of function_index 
-% j is column index of the i-th function index 
-% 
-% relevant output: switchingFcn.n, index of the function that was called in
-% function_index i,j; resp. the new created mtreeobj (either there is
-% another function call or there is a ctrlif to consider) 
+function switchingFcn = setUpSwitchingFunction_setUpFcnCall(switchingFcn, i, j)
+% In creating switching functions, a helper function may also need to be modified, in which case it
+% gets exported under a new name. This function adapts calls of the helper function from other functions
+% to the new name.
 
 cIndex = mtree_cIndex();
 
-n = switchingFcn.nCurrentFunction; 
+n = switchingFcn.nCurrentFunction;
 
 % find function call w.r.t to the function index
 function_index_i = switchingFcn.function_index_t1{i};
@@ -31,8 +22,8 @@ switchingFcn.name_old = cIndex_fcn{switchingFcn.mtreeobj_switchingFcn{3,n}.T(rIn
 
 switchingFcn.name_new = setUpSwitchingFunction_newName(switchingFcn, function_index_j);
 
-[switchingFcn, m] = setUpSwitchingFunction_checkForExistensOfSwitchingFunction(switchingFcn, switchingFcn.name_new, switchingFcn.name_old);
-switchingFcn.nCurrentFunction = m; 
+[switchingFcn, m] = setUpSwitchingFunction_findOrCreateSwitchingFcn(switchingFcn, switchingFcn.name_new, switchingFcn.name_old);
+switchingFcn.nCurrentFunction = m;
 
 [switchingFcn.mtreeobj_switchingFcn{3,n}, ~] = mtree_createAndAdd_NewNode(switchingFcn.mtreeobj_switchingFcn{3,n}, ...
     switchingFcn.mtreeobj_switchingFcn{5,n}.Call(u), ...                     % from
@@ -44,6 +35,4 @@ switchingFcn.mtreeobj_switchingFcn{3,n} = mtree_connectNodes(...
     switchingFcn.mtreeobj_switchingFcn{5,n}.Call(u),...
     switchingFcn.mtreeobj_switchingFcn{5,n}.Arg(u,3),...
     cIndex.indexRightchild);
-
-
 end 
