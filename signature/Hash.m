10961f145b1a4d064185c78c7983b00ad2809927
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2024-07-02 09:01:09 +0200

    WIP: Add class for signature and method to convert to string

---
+++ signature/Hash.m
@@ -0,0 +1,22 @@
+classdef Hash
+    %HASH Mixin class 
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function obj = Hash(inputArg1,inputArg2)
+            %HASH Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
