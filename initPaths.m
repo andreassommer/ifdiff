0331dafabbd1053c90efd4c3dab3927c05923c89
MichaelStrik <66440810+MichaelStrik@users.noreply.github.com>
2024-07-15 21:36:21 +0200

    initPaths adds new folder
    Since initPaths was last updated, new folders have been added to the project.
    initPaths now also adds these ones to the path

--- initPaths.m
+++ initPaths.m
@@ -11,5 +11,7 @@ function initPaths()
    addpath(genpath('./Examples'));  % include subdirs
    addpath(genpath('./PreprocessedFunctions'))
    addpath('./ComputeSensitivities');
+   addpath('./speedtracker');
+   addpath('./test');
    % addpath(genpath('./tinevez-matlab-tree-3d13d15'))
 end
