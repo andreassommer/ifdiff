7a560812a51747cf1c4873a905c7b52b9fe61eac
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-07-18 15:55:04 +0200

    remove unused properties from switchingFcn

--- setUpSwitchingFunction_assembleStruct.m
+++ setUpSwitchingFunction_assembleStruct.m
@@ -1,24 +1,13 @@
 function switchingFcn = setUpSwitchingFunction_assembleStruct(datahandle, i)
-% select and generate relavant information for assembling the
-% switchingfunction and store them a struct switchingfunction
-
-
+% create a struct for building the switching function and set a few properties in it
 data = datahandle.getData();
 
 switchingFcn.sI = data.SWP_detection.switchingIndices(i);
-switchingFcn.i = i;
-
 
 switchingFcn.mtreeobj = data.mtreeplus; 
 switchingFcn.mtreeobj_switchingFcn = cell(7,1); 
-% sI is the switching index, refered to the vectors in data.SWP_detection, get the ctrlif_index w.r.t. to sI
-% switchingfunction.rhs_new_name = setUpSwitchingFunction_newName(switchingfunction, switchingfunction.sI, i);
-
-% n-th switching functions refers to the n-th switch (n is suffix for switching function name)
-switchingFcn.n = length(data.SWP_detection.switchingpoints) + 1;
 
 switchingFcn.function_index_t1 = data.SWP_detection.function_index_t1;
-switchingFcn.function_index_t1_temp = 0;
 
 % every switching function gets a unique index and its functions index and
 % ctrlifindex. 
@@ -26,12 +15,9 @@ data.SWP_detection.uniqueSwEnumeration = data.SWP_detection.uniqueSwEnumeration 
 datahandle.setData(data); 
 switchingFcn.uniqueSwEnumeration = data.SWP_detection.uniqueSwEnumeration;
 
-
-
 switchingFcn.rhs_name_original = data.mtreeplus{2,1}; 
 
 % get the function_index w.r.t. to sI
-switchingFcn.function_index = data.SWP_detection.function_index_t1{switchingFcn.sI};
 switchingFcn.ctrlif_index = data.SWP_detection.ctrlif_index_t1;
 switchingFcn.condition = data.SWP_detection.switch_cond_t1;
 switchingFcn.rhs_name = setUpSwitchingFunction_newName(switchingFcn, 0);
