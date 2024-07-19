a70de3ef0985336b0f5cdeba270f820612d5448a
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-07-19 19:20:34 +0200

    feat: apply jump increments

--- extendODE_t2_to_tend_with_SWP_detection.m
+++ extendODE_t2_to_tend_with_SWP_detection.m
@@ -8,9 +8,16 @@ z = odextend(...
     data.SWP_detection.solution_until_t2,...
     @(t, y) data.integratorSettings.preprocessed_rhs(datahandle, t, y, data.SWP_detection.parameters), ...
     data.SWP_detection.tspan(2),...
-    [], ...
+    data.SWP_detection.x2{2}, ... conveniently, if this is identical to sol.y(end), odextend does not add a time point
     data.integratorSettings.optionsForcedBranching);
+
 data = datahandle.getData();
+% if a jump happened, shift t- by two eps to the left, so the solution is cadlag
+if ~isempty(data.SWP_detection.jumpFunction{end})
+    t2Index = length(data.SWP_detection.solution_until_t2.x);
+    t2 = z.x(t2Index);
+    z.x(t2Index) = t2 - 2*eps(t2);
+end
 data.SWP_detection.solution_until_t3 = z;
 
 % get timepoints: t_i, t_iplus1
