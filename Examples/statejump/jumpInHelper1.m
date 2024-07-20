82bb7c936a53653b5b7e6a4c7d717cd3809b1a71
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-07-20 06:47:22 +0200

    remove examples that are obsolete now that we have a proper test case

--- Examples/statejump/jumpInHelper1.m
+++
@@ -1,6 +0,0 @@
-function dx = jumpInHelper1(~, x, ~)
-    sigma = x(1) - 5;
-    dx = 1;
-    ifdiff_jumpif(sigma, 1, x(1));
-end
-
--- Examples/statejump/jumpInHelperRHS.m
+++
@@ -1,9 +0,0 @@
-function dx = jumpInHelperRHS(t, x, p)
-% Test RHS for jumps in helper functions. ISHYGDDT, but it should be supported.
-    if x(1) < 15
-        dx = jumpInHelper1(t, x, p);
-    else
-        dx = jumpInHelper2(t, x, p);
-    end
-end
-
