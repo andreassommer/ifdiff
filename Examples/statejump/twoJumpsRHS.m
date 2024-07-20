c6592d477928617455aae429a220fe6c40b9194f
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-07-20 06:47:22 +0200

    remove examples that are obsolete now that we have a proper test case

--- Examples/statejump/twoJumpsRHS.m
+++
@@ -1,8 +0,0 @@
-function dx = twoJumpsRHS(~, x, ~)
-% Example RHS where two jumps occur at the same SWP, which is wrong and should be
-% flagged appropriately by IFDIFF.
-% Start with a positive x value.
-    dx = -1;
-    ifdiff_jumpif(x(1), -1, [-10]);
-    ifdiff_jumpif(-x(1), 0, [-5]);
-end
