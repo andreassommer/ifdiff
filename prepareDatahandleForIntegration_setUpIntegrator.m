bdb08f56f820bd2abae669f71780a0fb65a763b8
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2024-07-31 12:44:56 +0200

    Add exception handling for integrator parameter

--- prepareDatahandleForIntegration_setUpIntegrator.m
+++ prepareDatahandleForIntegration_setUpIntegrator.m
@@ -2,7 +2,7 @@ function prepareDatahandleForIntegration_setUpIntegrator(datahandle, varargin)
 %PREPAREDATAHANDLEFORINTEGRATION_SETUPINTEGRATOR    Set function handles and optional arguments for integration.
 %   Store function handles in the datahandle:
 %   - The preprocessed RHS function
-%   - The OutputFcn called after each successful integration step to monitor switches.
+%   - The OutputFcn called after each successful integration step to monitor switches
 %   Set optional arguments or provide defaults for integration:
 %   - ODE Options (in particular AbsTol and RelTol)
 %   - The integrator to be used (as char array or function handle)
@@ -18,8 +18,8 @@ function prepareDatahandleForIntegration_setUpIntegrator(datahandle, varargin)
 data = datahandle.getData();
 
 % Default values
-data.integratorSettings.options = odeset( 'AbsTol', 1e-12, 'RelTol', 1e-3);
-data.integratorSettings.numericIntegrator = @ode15s;
+data.integratorSettings.options = odeset('AbsTol', 1e-12, 'RelTol', 1e-4);
+data.integratorSettings.numericIntegrator = @ode45;
 
 % Optional arguments
 if hasOption(varargin, 'options')
@@ -34,7 +34,18 @@ if hasOption(varargin, 'integrator')
     integrator = getOption(varargin, 'integrator');
     % Integrator may be passed as function handle or char array.
     if ~isa(integrator, 'function_handle')
-        integrator = str2func(integrator);
+        try
+            integrator = str2func(integrator);
+        catch ME
+            if strcmp(ME.identifier, 'MATLAB:string:MustBeStringScalarOrCharacterVector')
+                msg = sprintf( ...
+                    ['Incorrect type for parameter ''integrator'': ' ...
+                    'Expected text scalar or function handle but got %s instead.\n'], class(integrator));
+                causeException = MException('IFDIFF:prepareDatahandle:incorrectParameterType', msg);
+                ME = addCause(ME, causeException);
+            end
+            rethrow(ME);
+        end
     end
     data.integratorSettings.numericIntegrator = integrator;
 else
@@ -42,6 +53,7 @@ else
         'INFO: Parameter ''integrator'' not specified. Using default value: %s\n', ...
         func2str(data.integratorSettings.numericIntegrator));
 end
+
 
 % OutputFcn which will be called after each successful integration step to monitor switches.
 outputFunction =  @(t, y, flag) analyseSignature(t, y, flag, datahandle);
