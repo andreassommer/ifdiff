bfa5b9395d17fd58a01b60e1ccbc0b577d8d648f
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-08-16 19:44:30 +0200

    remove irrelevant scratch folder

--- scratch/forloop.m
+++
@@ -1,9 +0,0 @@
-function y = forloop(x)
-%FORLOOP reference function for what for loops' mtrees look like
-y = 0;
-for i = 1:x
-    z = y + 1;
-    y = z;
-end
-end
-
--- scratch/nestedFunction.m
+++
@@ -1,9 +0,0 @@
-function y = nestedFunction(x)
-    z = x + 1;
-    a = z * 2 * x;
-    function c = g(b)
-        c = b + 2;
-    end
-    y = g(a);
-end
-
