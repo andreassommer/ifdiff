3b104e0f573472b2d71f2edcea3f977b5458e923
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-08-23 10:24:57 +0200

    change state update in bounceball to match theoretical formulation; set time horizon to be near zeno limit

--- Examples/statejump/bounceballRHS.m
+++ Examples/statejump/bounceballRHS.m
@@ -16,5 +16,10 @@ function dx = bounceballRHS(~, x, p)
     dx = [x(2); -p(1)];
     % state jump: if the height goes from positive to negative, jump to [+eps, -v].
     % The eps is to avoid another immediate switching event being detected.
-    ifdiff_jumpif(x(1), -1, [-eps(1)*x(2) - x(1); -(1+p(2))*x(2)]);
+    % Refinement 1: set h+ = eps*v+^2. setting h+ to a value other than 0 kind of ruins that elegant
+    % geometric series, but setting h+ to a multiple of v+^2 lets us pull v+ out of the square root
+    % and get a geometric series after all.
+    % Refinement 2: instead of eps*v+^2, set h+ to (1/g)*eps*v+^2 because the error in v+ depends on
+    % g. this also leads to some nice canceling out in the derivation of the zeno limit
+    ifdiff_jumpif(x(1), -1, [eps(1)*(1/p(1)) * p(2)^2*x(2)^2 - x(1); -(1+p(2))*x(2)]);
 end
