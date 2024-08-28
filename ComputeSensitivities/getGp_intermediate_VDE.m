96b05be60d01e16e8f6c30308553f3a3b85a2efa
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-08-28 14:14:59 +0200

    fix: sensitivity computation fails if there is not at least one time point in every model stage

--- ComputeSensitivities/getGp_intermediate_VDE.m
+++ ComputeSensitivities/getGp_intermediate_VDE.m
@@ -1,9 +1,22 @@
 function Gp_values = getGp_intermediate_VDE(datahandle, sol, timepoints, modelNum, sensOptions)
 %GETGP_INTERMEDIATE_VDE approximate the parameter sensitivity Gp between two switching points
 % by solving the variational differential equation and return a cell array of Gp at the provided time points
+% timepoints must be sorted ascendingly, have no duplicates, and be within model #modelNum.
+    if isempty(timepoints)
+        Gp_values = {};
+        return;
+    end
     data = datahandle.getData();
     switches      = data.computeSensitivity.switches_extended;
     dim_y         = data.computeSensitivity.dim_y;
+    dim_p         = data.computeSensitivity.dim_p;
+
+    if switches(modelNum) == timepoints(end)
+        % ODE solvers will crash if you pass them identical start and end times, so we have to handle this special case
+        % Note that the conditions we placed on time points mean there is exactly one point in this case
+        Gp_values = {zeros(dim_y, dim_p)};
+        return;
+    end
 
     tspan    = [switches(modelNum), timepoints(end)];
     solVDE_p = solveVDE_Gp(datahandle, sol, tspan, modelNum, sensOptions);
