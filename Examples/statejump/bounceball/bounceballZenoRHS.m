6ec91dfc1810b0c6c1bc0233b18bcf5473a1f494
Kilian Folger <kilian.folger@uni-heidelberg.de>
2024-08-29 21:56:31 +0200

    add bouncing ball examle that solves zeno's phenomenon

--- Examples/statejump/bounceball/bounceballZenoRHS.m
+++ Examples/statejump/bounceball/bounceballZenoRHS.m
@@ -4,14 +4,22 @@ function dx = bounceballZenoRHS(~, x, p)
 % p = [g, gamma, eByMassMin]
 % See bounceballRHS for explanation on the basic bouncing ball.
 
-    % Energy computation: ePot = m*g*h, eKin = (1/2)*m*v^2. leaving out the mass since we
-    % divide by it afterward.
+    % Energy computation: ePot = m*g*h, eKin = (1/2)*m*v^2. leaving out the mass since the threshold for
+    % transitioning to the motionless model is defined by e/m, so we will end up dividing by the mass anyway.
     ePot = p(1) * x(1);
     eKin = (1/2) * x(2)^2;
-    if ePot + eKin < p(3)
+    e = ePot + eKin;
+    if e < p(3)
         dx = [0; 0];
     else
         dx = [x(2); -p(1)];
-        ifdiff_jumpif(x(1), -1, [eps(1)*(1/p(1)) * p(2)^2*x(2)^2 - x(1); -(1+p(2))*x(2)]);
+        if p(2)^2 * e < p(3)
+            ifdiff_jumpif(x(1), -1, -x);
+        else
+            ifdiff_jumpif(x(1), -1, [ ...
+                eps(1)*(1/p(1)) * p(2)^2*x(2)^2 - x(1); ...
+                -(1+p(2))*x(2) ...
+            ]);
+        end
     end
 end
