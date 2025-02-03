bd29db82ac8b9c18f3cbf8db1053f57292d44315
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-02-04 00:04:44 +0100

    Refactor: Simplify swapping children in mtree_switchLeftRightChildren

--- mtree_switchLeftRightChildren.m
+++ mtree_switchLeftRightChildren.m
@@ -5,20 +5,16 @@ function mtreeobj = mtree_switchLeftRightChildren(mtreeobj, index)
 %
 % INPUT:
 % 'mtreeobj'    mtree
-% 'index        Index of node of which the left/right children should be 
+% 'index        Index of node of which the left/right children should be
 %               swapped (mtree index).
 %
 % OUTPUT:
 % 'mtreeobj'    Edited mtree.
 
-cIndex = mtree_cIndex(); 
+cIndex = mtree_cIndex();
 
-% save indices of left and right child
-leftIndex   = mtreeobj.T(index, cIndex.indexLeftchild);
-rightIndex  = mtreeobj.T(index, cIndex.indexRightchild);
+left = cIndex.indexLeftchild;
+right = cIndex.indexRightchild;
 
-% swapping children
-mtreeobj.T(index, cIndex.indexLeftchild)    = rightIndex;
-mtreeobj.T(index, cIndex.indexRightchild)   = leftIndex;
-
+mtreeobj.T(index, [left, right]) = mtreeobj.T(index, [right, left]);
 end
