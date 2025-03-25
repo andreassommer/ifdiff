7ccdd194bc204a70fecfb55ea7ae2a779aa93819
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-03-25 12:47:13 +0100

    Add comments for preprocessed function data class

--- signature/PreprocessedFunctionData.m
+++ signature/PreprocessedFunctionData.m
@@ -1,5 +1,6 @@
 classdef PreprocessedFunctionData < handle
-    %PREPROCESSEDFUNCTIONDATA Summary of this class goes here
+    %PREPROCESSEDFUNCTIONDATA   Manages shared preprocessed function data used for switching function generation.
+
     properties (Access=public)
         mtreeArray = {}
         functionNameArray = {}
@@ -12,7 +13,6 @@ classdef PreprocessedFunctionData < handle
     end
 
     methods
-        % Constructor
         function obj = PreprocessedFunctionData(mtreeArray, functionNameArray)
             obj.functionIndexToIdxMtreeMap = containers.Map('KeyType', 'int32', 'ValueType', 'int32');
 
@@ -28,6 +28,10 @@ classdef PreprocessedFunctionData < handle
         end
 
         function idxMtree = functionIndexToIdxMtree(this, functionIndex, idxCallerMtree)
+            % Given the index of an mtree and a function index of a call contained within that mtree,
+            % determine the index of the mtree of the function corresponding to that function index.
+
+            % Check if we have previously stored the result of this query already
             if this.functionIndexToIdxMtreeMap.isKey(functionIndex)
                 idxMtree = this.functionIndexToIdxMtreeMap(functionIndex);
                 return
@@ -39,27 +43,34 @@ classdef PreprocessedFunctionData < handle
             this.functionIndexToIdxMtreeMap(functionIndex) = idxMtree;
         end
 
-        function helperFunctionCallInfo = getHelperFunctionCallInfo(this, idxMtree, varargin)
-            helperFunctionCallInfo = this.helperFunctionCallInfoArray{idxMtree};
-            if isempty(helperFunctionCallInfo)
-                helperFunctionCallInfo = this.createHelperFunctionCallInfo(this.mtreeArray{idxMtree});
-                % Cache result for future access
-                this.helperFunctionCallInfoArray{idxMtree} = helperFunctionCallInfo;
-            end
-
-            if nargin > 0
-                functionIndex = varargin{1};
-                helperFunctionCallInfo = helperFunctionCallInfo.setActiveIndex(functionIndex);
-            end
-        end
-
         function functionName = functionIndexToFunctionName(this, functionIndex, idxCallerMtree)
+            % Given the index of an mtree and a function index of a call contained within that mtree,
+            % determine the name of the function corresponding to that function index.
             cIndex = mtree_cIndex;
 
             helperFunctionCallInfo = this.getHelperFunctionCallInfo(idxCallerMtree, functionIndex);
 
             callerMtree = this.mtreeArray{idxCallerMtree};
             functionName = callerMtree.C{callerMtree.T(helperFunctionCallInfo.rIndexFname, cIndex.stringTableIndex)};
+        end
+
+        function helperFunctionCallInfo = getHelperFunctionCallInfo(this, idxMtree, varargin)
+            % Given the index of an mtree and optionally a function index,
+            % retrieve the relevant rIndex entries for helper function calls in the mtree.
+            
+            % Check if the helper function call info was generated previously for this mtree.
+            helperFunctionCallInfo = this.helperFunctionCallInfoArray{idxMtree};
+            if isempty(helperFunctionCallInfo)
+                % Create new and cache result for future access.
+                helperFunctionCallInfo = this.createHelperFunctionCallInfo(this.mtreeArray{idxMtree});
+                this.helperFunctionCallInfoArray{idxMtree} = helperFunctionCallInfo;
+            end
+            
+            % Optionally, restrict access to only one function index.
+            if nargin > 0
+                functionIndex = varargin{1};
+                helperFunctionCallInfo = helperFunctionCallInfo.setActiveIndex(functionIndex);
+            end
         end
     end
 
@@ -131,6 +142,6 @@ classdef PreprocessedFunctionData < handle
             else
                 helperFunctionCallInfo.functionIndex = updateFunctionIndexArg2;
             end
-        end    
+        end
     end
 end
