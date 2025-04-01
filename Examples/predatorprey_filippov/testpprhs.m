572064459401fe580ed892b693c698e23de7d5c0
MichaelStrik <66440810+MichaelStrik@users.noreply.github.com>
2025-04-01 15:07:27 +0200

    examples: predator prey filippov got more visualization

--- Examples/predatorprey_filippov/testpprhs.m
+++ Examples/predatorprey_filippov/testpprhs.m
@@ -1,4 +1,3 @@
-
 % parameter values p = (r1, r2, beta1, beta2, q1, q2, m, e)
 % order as in paper (see rhs file)
 m     = 0.790;
@@ -12,46 +11,53 @@ r2    = 0.126;
 beta1 = 7.8;  % repulsive focus, below curve mu
 
 beta1 = 7.81; q2 = 1.5; r2 = 0.3;
-p = [r1, r2, beta1, beta2, q1, q2, m, e, aq];
+
 
 % initial values
 a = 0.286975;
 x0_1 = [a ; a ; r1-r2];
 x0_2 = x0_1 + [0.001 ; 0.0001 ; 0];
 
-
-tspanA = [0 150];%2*[0 150];
+p = [r1, r2, beta1, beta2, q1, q2, m, e, aq];
+tspanA = [0 100];%2*[0 150];
 tspanB = 2*[0 135];
-opts = odeset('reltol', 1e-6, 'abstol', 1e-9, 'MaxStep', 0.1, 'InitialStep', 0.5*1e-4);
+eulerStep = 1e-5;
 
-doOriginal = false;
+opts = odeset('reltol', 1e-6, 'abstol', 1e-9, 'MaxStep', 0.1);
+
+doOriginal = true;
 doTransformed = false;
 doIfdiff = true;
+analyzeResults = true; % compare ifdiff and original
 
-integrator = @euler;
+integrator = @explEuler;
 
-% ORIGINAL SYSTEM: simple simlation with ode23: TAKES VERY LONG !!!
+% plain integration, no treatment of switches
+fignum = 333;
 if doOriginal
-   sol = integrator(@(t,x) pprhs(t,x,p), tspanA, x0_1, opts);
-   plotit(333, sol.x, sol.y, 'Original', integrator, 'b');
+   origSol = integrator(@(t,x) pprhs(t,x,p), tspanA, x0_1, eulerStep);
+   plotit(fignum, origSol.x, origSol.y, 'b');
 end
 
 if doIfdiff
     filename = 'pprhs';
-    datahandle = prepareDatahandleForIntegration(filename, 'solver', func2str(@ode45), 'options', opts);
-    sol = solveODE(datahandle, tspanA, x0_1, p);
-    plotit(333, sol.x, sol.y, 'Original', @ode45, 'b');
+    ifdiffIntegrator = @ode45;
+    datahandle = prepareDatahandleForIntegration(filename, 'solver', func2str(ifdiffIntegrator), 'options', opts);
+    %profile off; profile clear; profile on
+    ifdiffSol = solveODE(datahandle, tspanA, x0_1, p);
+    % profile off; profile viewer
+    plotit(fignum, ifdiffSol.x, ifdiffSol.y, 'g');
 end
 
 % TRANSFORMED SYSTEM
 if doTransformed
-   fignum = 333; myfig = figure(fignum); clf(fignum);
+   myfig = figure(fignum); clf(fignum);
    solA = integrator(@(t,x) pprhs5(t,x,p), tspanA, x0_1, opts);
    solB = integrator(@(t,x) pprhs5(t,x,p), tspanB, x0_2, opts);
    xA=solA.x; yA=solA.y; xB=solB.x; yB=solB.y;
    
-   plotit(fignum, xA, yA, 'Transformed', integrator, 'b');
-   plotit(fignum, xB, yB, 'Transformed', integrator, 'g');
+   plotit(fignum, xA, yA, 'b');
+   plotit(fignum, xB, yB, 'g');
    xlabel('y1'); ylabel('y2'); zlabel('y3');
    fitplane(yB)
    set(gca, 'YDir','reverse')
@@ -61,12 +67,30 @@ if doTransformed
    
 end
 
+if doOriginal || doIfdiff || doTransformed
+    datalabels = {};
+    if doOriginal,      datalabels{end+1} = ['plain ', func2str(integrator)];       end %#ok<UNRCH> 
+    if doIfdiff,        datalabels{end+1} = ['ifdiff+',func2str(ifdiffIntegrator)]; end %#ok<UNRCH> 
+    if doTransformed,   datalabels{end+1} = 'Transformed';                          end %#ok<UNRCH>
+    legend(datalabels{:});
+end
+
+if analyzeResults
+    initStep = eulerStep;
+    evalEvery = floor(1/initStep*1/10);
+    solTime = origSol.x(1:evalEvery:end);
+    solDiff = vecnorm(origSol.y(:,1:evalEvery:end) - deval(ifdiffSol, solTime)); %#ok<UNRCH> 
+    figDiff = figure(fignum+1); 
+    semilogy(solTime, solDiff, 'LineWidth', 1.0);
+    xlabel('t');
+    ylabel('||y||_2');
+    legend(['difference plain ', func2str(integrator), ' and ifdiff+', func2str(ifdiffIntegrator)]);
+end
 
 % FINITO
 
 
 % HELPER
-
 function p = fitplane(YY)
    x = YY(3,:);
    y = YY(2,:);
@@ -83,19 +107,19 @@ function p = fitplane(YY)
 end
 
 
-function plotit(fignum, x, y, system, integrator, color)
+function plotit(fignum, x, y, color)
    figure(fignum); hold on;
    plot3(y(3,:), y(2,:), y(1,:), color, 'LineWidth', 3.0);%
-   %title(sprintf('Transformed - Integrator: %s', func2str(integrator)));
    view([166 12]); 
    grid on;
    box on;
+   xlabel('Predator');
+   ylabel('prey2');
+   zlabel('prey1');
    
 end
 
-
-function sol = euler(rhs, tspan, x0, opts)
-   stepsize = opts.InitialStep;
+function sol = explEuler(rhs, tspan, x0, stepsize)
    xdim = length(rhs(0,x0));  % get dimension
    T = linspace(tspan(1), tspan(end), (tspan(end)-tspan(1))/stepsize);
    steps = length(T);
@@ -111,7 +135,3 @@ function sol = euler(rhs, tspan, x0, opts)
    sol.x = T;
    sol.y = X';
 end
-
-
-
-
