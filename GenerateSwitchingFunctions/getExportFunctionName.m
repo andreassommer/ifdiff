d8cc429d908d419e7dcfb2b91ea12590ec196140
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-04-22 12:48:43 +0200

    Improve comments and readability in get export function name

--- GenerateSwitchingFunctions/getExportFunctionName.m
+++ GenerateSwitchingFunctions/getExportFunctionName.m
@@ -1,4 +1,24 @@
-function name = getExportFunctionName(rhsName, exportIndex)
+function name = getExportFunctionName(functionName, exportIndex)
+%name = GETEXPORTFUNCTIONNAME(functionName, exportIndex)
+%
+%Get the name of an individual (helper) function from the name of a switching/jump function.
+%Note that the RHS is always assigned index 1 and helper functions are numbered sequentially.
+%
+%INPUT:
+%   functionName - Name of the switching/jump function.
+%       char array
+%
+%   exportIndex - Index of the (helper) function in export array assigned during creation of switching/jump function.
+%       positive integer
+%
+%OUTPUT:
+%   name - Name of the individual function based on the following format:
+%   <functionName><DELIM><exportIndex>
+%       char array
+%
+%See also CREATESWITCHINGFUNCTIONNAME
+
 DELIMITER = '_';
-name = [rhsName, DELIMITER, num2str(exportIndex)];
+
+name = [functionName, DELIMITER, num2str(exportIndex)];
 end
