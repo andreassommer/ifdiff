d217a67b68253c1d04a582cb02adb5f1ee30f6cf
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-04-22 11:49:47 +0200

    Improve comments and readability in switching function factory

--- GenerateSwitchingFunctions/@SwitchingFunctionFactory/SwitchingFunctionFactory.m
+++ GenerateSwitchingFunctions/@SwitchingFunctionFactory/SwitchingFunctionFactory.m
@@ -1,24 +1,44 @@
-classdef SwitchingFunctionFactory < handle
-    %SWITCHINGFUNCTIONFACTORY   Retrieve existing switching/jump functions from signature or create new ones
+classdef SwitchingFunctionFactory
+    %this = SWITCHINGFUNCTIONFACTORY(functionData, writePath, namePrefix, outputName)
+    %
+    %Retrieve existing switching/jump functions from signature or create new ones.
+    %
+    %INPUT:
+    %   functionData - Data related to preprocessed functions used to create new switching/jump functions.
+    %       PreprocessedFunctionData
+    %
+    %   writePath - Absolute path to directory in which the source code of new functions will be placed.
+    %       char array
+    %
+    %   namePrefix - Prefix added to the name of functions.
+    %   Primarily used to differentiate between switching and jump functions (e.g. sw_ and jump_).
+    %       char array
+    %
+    %   outputName - Name of the output variable for functions.
+    %   Primarily used to differentiate between switching and jump functions (e.g. switching_value and jump_increment).
+    %       char array
+    %
+    %See also PreprocessedFunctionData, SwitchingFunctionSignature
+
     properties (Access=public)
+        functionData
         writePath = ''
         namePrefix = ''
         outputName = ''
-        functionData = []
     end
 
     methods
         % Constructor
-        function obj = SwitchingFunctionFactory(functionData, writePath, namePrefix, outputName)
+        function this = SwitchingFunctionFactory(functionData, writePath, namePrefix, outputName)
             if nargin == 0
+                this.functionData = PreprocessedFunctionData();
                 return
             end
-            % Object of type PreprocessedFunctionData
-            obj.functionData = functionData;
 
-            obj.writePath = writePath;
-            obj.namePrefix = namePrefix;
-            obj.outputName = outputName;
+            this.functionData = functionData;
+            this.writePath = writePath;
+            this.namePrefix = namePrefix;
+            this.outputName = outputName;
         end
 
         switchingFunctionHandle = create(this, signature, collisionIndex, varargin);
