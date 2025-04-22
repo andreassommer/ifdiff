5e7a7780197590882db1a13d52b94a1370395bb7
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-04-22 12:40:44 +0200

    Improve comments and readability in create switching function name

--- GenerateSwitchingFunctions/createSwitchingFunctionName.m
+++ GenerateSwitchingFunctions/createSwitchingFunctionName.m
@@ -1,14 +1,34 @@
-function newFunctionName = createSwitchingFunctionName(prefix, oldFunctionName, hash, varargin)
+function newName = createSwitchingFunctionName(prefix, rhsName, hash, collisionIndex)
+%newName = CREATESWITCHINGFUNCTIONNAME(prefix, rhsName, hash, collisionIndex)
+%
+%Create a new unique name for a switching/jump function.
+%
+%INPUT:
+%   prefix - Prefix added to the function name.
+%   Primarily used to differentiate between switching and jump functions (e.g. sw_ and jump_)
+%       char array
+%
+%   rhsName - Name of the RHS function.
+%       char array
+%
+%   hash - Hash computed from a switching function signature.
+%       char array
+%
+%   collisionIndex - Index used to handle hash collisions in the signature.
+%       positive integer
+%
+%OUTPUT:
+%   newName - Unique name for a new switching/jump function based on the following format:
+%   <prefix><oldName><DELIM><hash><DELIM><collisionIndex>
+%       char array
+
 DELIMITER = '_';
 
-collisionIndex = '';
-if nargin > 3
-    collisionIndex = num2str(varargin{1});
-end
+collisionIndex = num2str(collisionIndex);
 
-newFunctionName = [ ...
+newName = [ ...
     prefix, ...
-    oldFunctionName, DELIMITER, ...
+    rhsName, DELIMITER, ...
     hash, DELIMITER, ...
     collisionIndex];
 end
