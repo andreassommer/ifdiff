7af20af98d63787ee0e84d73f441ffd0c625a7e3
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-05-04 19:11:41 +0200

    Make SwitchingFunctionSignature cache string and hash

--- GenerateSwitchingFunctions/SwitchingFunctionSignature.m
+++ GenerateSwitchingFunctions/SwitchingFunctionSignature.m
@@ -38,6 +38,12 @@ classdef SwitchingFunctionSignature
         hash
     end
 
+    properties (Access=private, Hidden)
+        doCacheUpdate = false
+        strCached = ''
+        hashCached = ''
+    end
+
     properties (Constant, Hidden)
         RHS_NAME_SUFFIX = ':';
         SWITCH_COND_SUFFIX = '/';
@@ -58,10 +64,13 @@ classdef SwitchingFunctionSignature
             this.switchCond = switchCond;
             this.ctrlifIndex = ctrlifIndex;
             this.functionIndex = functionIndex;
+
+            this.doCacheUpdate = true;
+            this = this.updateCache();
         end
 
-        function str = get.str(this)
-            %str = this.str
+        function str = toStr(this)
+            %str = this.TOSTR()
             %
             %Concatenate data properties of signature into a unique string.
             %
@@ -91,11 +100,53 @@ classdef SwitchingFunctionSignature
                 functionIndexString];
         end
 
-        function hash = get.hash(this)
-            %hash = this.hash
+        function hash = toHash(this)
+            %hash = this.TOHASH()
             %
             %Compute 32-bit hash of string representation of signature and output in hex format
+
             hash = dec2hex(fnv(this.str), 8);
+        end
+
+        function this = updateCache(this)
+            %this = this.UPDATECACHE()
+            %
+            %Update the cached values for str and hash.
+
+            if this.doCacheUpdate
+                this.strCached = this.toStr();
+                this.hashCached = this.toHash();
+            end
+        end
+
+        % Getters retrieve the cached value for str and hash.
+        function str = get.str(this)
+            str = this.strCached;
+        end
+
+        function hash = get.hash(this)
+            hash = this.hashCached;
+        end
+
+        % Setters have to update the cached values for str and hash.
+        function this = set.rhsName(this, val)
+            this.rhsName = val;
+            this = this.updateCache();
+        end
+
+        function this = set.switchCond(this, val)
+            this.switchCond = val;
+            this = this.updateCache();
+        end
+
+        function this = set.ctrlifIndex(this, val)
+            this.ctrlifIndex = val;
+            this = this.updateCache();
+        end
+
+        function this = set.functionIndex(this, val)
+            this.functionIndex = val;
+            this = this.updateCache();
         end
     end
 end
