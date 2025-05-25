a37e9d7801064cd38640d51908100ab8a816bf96
Marvin <vu252@stud.uni-heidelberg.de>
2025-05-25 23:29:10 +0200

    Startet a new test. Canonical Example with VDEs as an extended system 	new file:   Examples/canExWithSensSystem/ExtendedCanonicalSysTest.m 	new file:   Examples/canExWithSensSystem/ExtendedCanonicalSys_rhs.m

---
+++ Examples/canExWithSensSystem/ExtendedCanonicalSys_rhs.m
@@ -0,0 +1,17 @@
+% So far the extension for the system is completely missing. Will be added
+% later
+
+function dx = ExtendedCanonicalSys_rhs(t,x,p)
+
+dx = zeros(2,1);
+dx(1) = 0.01 * t.^2  +  x(2).^3;
+
+if x(1) < p(1) 
+    dx(2) = 0;
+else
+    if x(1) < p(1) + 0.5
+        dx(2) = 5;
+    else
+        dx(2) = 0;
+    end
+end
