cb5cd1f108f7207d5b8592a581cffd1c834fdbbd
MichaelStrik <66440810+MichaelStrik@users.noreply.github.com>
2025-06-09 14:50:30 +0200

    calls new improved functions + some minor improvements

--- solveODE_backtrackChattering.m
+++ solveODE_backtrackChattering.m
@@ -27,42 +27,44 @@ function [t, sliding_index, signatures] = solveODE_backtrackChattering(datahandl
 
     % get switching data
     SWP_detection   = data.SWP_detection;
-    switchingpoints = SWP_detection.switchingpoints;
+    switchingpoints = cell2mat(SWP_detection.switchingpoints);
 
     % compute statistical measures of switching frequencies
     % based on the last 'checklast' switches
-    switching_frequencies = diff(cell2mat(switchingpoints));
-    swfreq_mean = mean(switching_frequencies(end-checklast+2:end));
-    swfreq_var  = var(switching_frequencies(end-checklast+2:end));
+    switching_frequencies = diff(switchingpoints);
+    [swfreq_var, swfreq_mean]  = var(switching_frequencies(end-checklast+2:end));
     swfreq_sd   = sqrt(swfreq_var);
 
     % determine to which switch to go back
     % Idea: Go back until we see the time-distance between switches
     % becoming much higher. Use sample standard deviation for decision.
     n = length(switching_frequencies);
-    t = switchingpoints{1}; 
+    t = switchingpoints(1); 
     % t is the algorithmically determined starting time of the chattering
+    j = n+2;
     for i=n:-1:1
         if switching_frequencies(i) > swfreq_mean+3*swfreq_sd
-            t = switchingpoints{i+1};
+            t = switchingpoints(i+1);
+            j = i+1;
             break;
         end
     end
 
     % Determine the chattering switches (or rather the signatures involved)
-    switchingpoints = cell2mat(switchingpoints);
-    chattering_switchingpts = switchingpoints(switchingpoints >=t);
-    k = length(chattering_switchingpts);
+    chattering_switchingpoints = switchingpoints(j:end);
+    k = length(chattering_switchingpoints);
     switchingFunctions = SWP_detection.switchingFunction(end-k+1:end);
-    
-    rhs_name = func2str(data.integratorSettings.preprocessed_rhs);
-    signatures = getSignatureFromHandle(switchingFunctions, rhs_name);
-    
+
+    signatures = getUniqueSignaturesFromHandle(switchingFunctions);
+    rhs_name = data.mtreeplus{2,1};
+    cut_rhs_name = @(sig) sig(length(rhs_name)+2:end); % +2 because rhs name and signature are seperated by a colon
+    signatures = cellfun(cut_rhs_name, signatures, 'UniformOutput', false);
+
     if length(signatures) > 2 % more than one switch involved --> not supported
         errMsg = 'Encountered chattering that involves more than one switch. Cannot solve.\n';
         error('IFDIFF:chattering', errMsg);
     end
 
     sliding_index = SWP_detection.switchingIndices(end);
-    
+
 end
