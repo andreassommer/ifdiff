c87293b092e1420181482f891c5b9fb5c360c675
MichaelStrik <66440810+MichaelStrik@users.noreply.github.com>
2025-06-25 18:24:10 +0200

    Refactor: Created functions to eliminate code duplicates

--- solveODE_cutSteps_solution_until_t2.m
+++ solveODE_cutSteps_solution_until_t2.m
@@ -1,11 +1,12 @@
 function solveODE_cutSteps_solution_until_t2(datahandle, t_cut)
-% Removes k last integration step from ode sol object in datahandle.
+% Removes integration step that exceed time 't_cut' from ode sol object in 
+% datahandle.
 % 
 % INPUT:
 % 'datahandle': datahandle containing the integration and switching data.
 %                   handle
 %
-% 't':          timepoint until which is to be cut
+% 't_cut':      timepoint until which is to be cut (exclusive)
 %                   double
 %
 % OUTPUT:
@@ -18,44 +19,10 @@ function solveODE_cutSteps_solution_until_t2(datahandle, t_cut)
 %
 % Code adapted from solveODE_solution_until_t1.m
 
-
 data = datahandle.getData(); 
 
-% Step 1: Cut off steps in solution object
-
-% cut off steps past the timepoint t
-solution_until_t2 = data.SWP_detection.solution_until_t2;
-t = solution_until_t2.x; % x: time points
-
-data.SWP_detection.solution_until_t2.x(t>t_cut)     = [];
-data.SWP_detection.solution_until_t2.y(:, t>t_cut)  = [];
-
-% adjust the interpolation data - depends on the used solver
-solver = data.SWP_detection.solution_until_t2.solver;
-switch solver
-    case {'ode23', 'ode45', 'ode78', 'ode89'}
-        data.SWP_detection.solution_until_t2.idata.f3d(:, :, t>t_cut)   = [];
-
-    case 'ode15s'
-        data.SWP_detection.solution_until_t2.idata.kvec(t>t_cut)        = [];
-        data.SWP_detection.solution_until_t2.idata.dif3d(:, :, t>t_cut) = [];
-    
-    case 'ode113'
-        data.SWP_detection.solution_until_t2.idata.klastvec(t>t_cut)    = [];
-        data.SWP_detection.solution_until_t2.idata.phi3d(:, :, t>t_cut) = [];
-        data.SWP_detection.solution_until_t2.idata.psi2d(:, t>t_cut)    = []; 
-
-    case 'ode23t'
-        data.SWP_detection.solution_until_t2.idata.z(:, t>t_cut)    = [];
-        data.SWP_detection.solution_until_t2.idata.znew(:, t>t_cut) = [];
-
-    case 'ode23tb'
-        data.SWP_detection.solution_until_t2.idata.t2(t>t_cut)      = [];
-        data.SWP_detection.solution_until_t2.idata.y2(:, t>t_cut)   = [];
-
-    otherwise
-        error('The solver is not known to the programm - can not continue.');
-end
+% Step 1: In solution object, cut steps past time t_cut
+cutSteps_solution(datahandle, 'solution_until_t2', t_cut);
 
 % Step 2: Adjust the switching data
 SWP_detection = data.SWP_detection;
@@ -86,5 +53,9 @@ data.SWP_detection.signature.function_index = data.SWP_detection.signature.funct
 data.SWP_detection.signature.switch_cond    = data.SWP_detection.signature.switch_cond([true, switchingpoints<=t_cut]);
 
 datahandle.setData(data);
+
+% adjust convexification data
+cutSteps_convexification(datahandle, t_cut)
+
 
 end
