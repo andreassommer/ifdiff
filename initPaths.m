ad4bb843fa592d964eab79a53479a1d83569210f
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-07-08 09:38:39 +0200

    Rename dir for generating switching/jump/model functions

--- initPaths.m
+++ initPaths.m
@@ -19,5 +19,5 @@ addpath(genpath(fullfile(filePath, 'Tools')));
 addpath(genpath(fullfile(filePath, 'Examples')));
 addpath(genpath(fullfile(filePath, 'PreprocessedFunctions')));
 addpath(fullfile(filePath, 'ComputeSensitivities'));
-addpath(genpath(fullfile(filePath, 'GenerateSwitchingFunctions')));
+addpath(genpath(fullfile(filePath, 'GenerateFixedBranchingFunctions')));
 end
