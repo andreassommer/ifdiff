741239870bd2c300530b3701993bd268358f9498
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-07-12 14:41:08 +0200

    Add comments and refactor mtree helper functions

--- mtree_getOutputNames.m
+++ mtree_getOutputNames.m
@@ -1,4 +1,20 @@
 function outputNames = mtree_getOutputNames(mtree, rIndex)
+%outputNames = MTREE_GETOUTPUTNAMES(mtree, rIndex)
+%
+%Get names of all outputs of a function defined in an mtree.
+%
+%INPUT:
+%   mtree - Mtree containing the function definition (as its main function, i.e. not local or nested).
+%       mtreeplus
+%
+%   rIndex - Important row indices of the mtree.
+%   If an empty array is passed, the row index will be generated in this function.
+%       struct | empty array
+%
+%OUTPUT:
+%   outputNames - Names of the function outputs.
+%       1xN cell array of 1x? char array
+
 cIndex = mtree_cIndex();
 
 if isempty(rIndex)
--- mtree_getReturnStatement.m
+++ mtree_getReturnStatement.m
@@ -1,24 +1,44 @@
 function rIndexReturn = mtree_getReturnStatement(mtree)
-rIndex = struct('HEAD', struct(), 'BODY', struct()); 
+%rIndexReturn = MTREE_GETRETURNSTATEMENT(mtree)
+%
+%Get row index of the last top-level statement modifying the output of a function.
+%
+%INPUT:
+%   mtree - Mtree containing the function definition (as its main function, i.e. not local or nested).
+%       mtreeplus
+%
+%OUTPUT:
+%   rIndexReturn - Row index of the "return statement" in the mtree.
+%       positive integer
+
+rIndex = struct('HEAD', struct(), 'BODY', struct());
 rIndex.HEAD = mtree_rIndex_head(mtree, rIndex.HEAD);
 
 outputNames = mtree_getOutputNames(mtree, rIndex);
-rIndexOutputUsage = mtree.mtfind('String', outputNames).indices();
-rIndexReturn = getFunctionBodyNode(mtree, rIndexOutputUsage(end));
+if ~isscalar(outputNames)
+    throw(invalidNumberOfOutputs(outputNames));
 end
 
-function node = getFunctionBodyNode(mtree, node)
-% Given an mtree of a function and a node, find the body node (= top-level statement)
-% that the node belongs to.
-% This is useful e.g. when the return statement is within an if block. Then
-% deleteUnusedParameters_walkBodyNodes needs the index of the if block, not of the
-% return statement.
+rIndexOutputUsage = mtree.mtfind('String', outputNames).indices();
+rIndexReturn = getFunctionBodyNode(mtree, rIndexOutputUsage(end), rIndex.HEAD);
+end
+
+%% Helpers
+function node = getFunctionBodyNode(mtree, node, rIndexHead)
+% Find body node (= top-level statement) of a node in an mtree.
+% E.g. to find the index of an if-block for a return statement within an if-block.
+
 cIndex = mtree_cIndex();
-rIndexHead = mtree_rIndex_head(mtree);
 
 trueParent = mtree.T(node, cIndex.trueParent);
 while trueParent ~= rIndexHead.FUNCTION
     node = mtree.T(node, cIndex.indexParentNode);
     trueParent = mtree.T(node, cIndex.trueParent);
 end
+end
+
+%% Exceptions
+function e = invalidNumberOfOutputs(outputNames)
+msg = 'Unable to determine return statement: Number of function outputs should be 1, not %d.\n';
+e = MException('IFDIFF:MtreeFindReturnStatement:InvalidNumberOfOutputs', msg, length(outputNames));
 end
