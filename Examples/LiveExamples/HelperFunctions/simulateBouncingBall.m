01469e4fbe4f0d1bc31ce5ade37901dd1afea874
Marvin <vu252@stud.uni-heidelberg.de>
2025-07-18 01:42:17 +0200

    new file:   Examples/LiveExamples/CanonicalSensitivities.mlx 	new file:   Examples/LiveExamples/HelperFunctions/CanonicalSensPlot.m 	new file:   Examples/LiveExamples/HelperFunctions/bounceballRHS_cp.m 	new file:   Examples/LiveExamples/HelperFunctions/plot_canon_sens_p.m 	new file:   Examples/LiveExamples/HelperFunctions/plot_canonical_comparison.m 	new file:   Examples/LiveExamples/HelperFunctions/plot_comparison_subplots.m 	new file:   Examples/LiveExamples/HelperFunctions/run_accurate_euler.m 	new file:   Examples/LiveExamples/HelperFunctions/simulateBouncingBall.m 	new file:   Examples/LiveExamples/HelperFunctions/whiteCabbagePlotLive.m 	new file:   Examples/LiveExamples/StateJumpExample.mlx 	new file:   Examples/LiveExamples/WhiteCabbageLive.mlx 	new file:   Examples/LiveExamples/canonicalExRHS_test_live.mlx

---
+++ Examples/LiveExamples/HelperFunctions/simulateBouncingBall.m
@@ -0,0 +1,20 @@
+function simulateBouncingBall(t0, tEnd, x0, p, solver, options)
+    datahandle = prepareDatahandleForIntegration('bounceballRHS', 'solver', func2str(solver), 'options', options);
+    sol = solveODE(datahandle, [t0 tEnd], x0, p);
+
+    FDstep = generateFDstep(size(x0, 1), length(p));
+    sensFun = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'VDE', 'CalcGy', true, 'CalcGp', true);
+
+    
+    T = t0:0.01:tEnd;
+    H = deval(sol, T, 1); % Height
+    V = deval(sol, T, 2); % Velocity
+    E = p(1) * H + 0.5 * V.^2; % Energy per mass
+
+    plot(T, H, 'LineWidth', 2, 'DisplayName', 'h(t)', 'Color', [0 0.5 0.7]);
+    hold on;
+    plotSolWithJumps([t0 tEnd], sol, 2, 'v(t)', [0.2 0.7 0], struct(), 'LineWidth', 2);
+    plot(T, E, 'LineWidth', 2, 'DisplayName', 'e(t)/m', 'Color', [0.6 0.6 0]);
+    hold off;
+    plotSensitivitiesSwitched(sol, sensFun, [t0 tEnd], struct(), struct(), 'LineWidth', 2);
+end
