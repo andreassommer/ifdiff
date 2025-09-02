4dcae3f555fbcfb9f71ae8d908c4db2a28871cf5
pilar <pilar@pilar.domain>
2025-09-02 21:33:01 +0200

    Updatd plots in sensitivities for the canonical example

--- Examples/canonicalExample/sensitivities_CanonicalExample.m
+++ Examples/canonicalExample/sensitivities_CanonicalExample.m
@@ -17,8 +17,8 @@ FDstep = generateFDstep(dim_y, dim_p, 'hy_rel_flag', true,'hp_rel_flag', true, '
 %% Generation of sensitivity functions
 tic;
 integrator_options = odeset( 'AbsTol', 1e-14,'RelTol', 1e-12);
-%sensitivities_function_ENDfull = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_full', 'calcGy', true,'calcGp',true, 'Gmatrices_intermediate', true, 'save_intermediates', true);
-%sensitivities_function_ENDpiece = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_piecewise', 'calcGy', true,'calcGp',true, 'Gmatrices_intermediate', true, 'save_intermediates', false);
+sensitivities_function_ENDfull = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_full', 'calcGy', true,'calcGp',true, 'Gmatrices_intermediate', true, 'save_intermediates', true);
+sensitivities_function_ENDpiece = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_piecewise', 'calcGy', true,'calcGp',true, 'Gmatrices_intermediate', true, 'save_intermediates', false);
 sensitivities_function_VDE = generateSensitivityFunction(datahandle, sol, FDstep, 'integrator_options', integrator_options, 'method', 'VDE', 'calcGy', true,'calcGp',false, 'Gmatrices_intermediate', true, 'save_intermediates', false);
 toc;
 
@@ -29,26 +29,30 @@ t = [17,18];
 %t = 0:0.1:20;
 
 tic;
-%sensitivities_full = sensitivities_function_ENDfull(t);
-%sensitivities_piece = sensitivities_function_ENDpiece(t);
+sensitivities_full = sensitivities_function_ENDfull(t);
+sensitivities_piece = sensitivities_function_ENDpiece(t);
 sensitivities_VDE = sensitivities_function_VDE(t);
 toc;
 
 %% Plot sensitivities initial values
-%sensitivities_function_ENDpiecewise = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_piecewise', 'Gy', true,'Gp',false, 'Gmatrices_intermediate', true, 'save_intermediates', true);
+sensitivities_function_ENDpiecewise = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_piecewise', 'Gy', true,'Gp',false, 'Gmatrices_intermediate', true, 'save_intermediates', true);
 sensitivities_function_VDE = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'VDE', 'calcGy', true,'calcGp',false, 'Gmatrices_intermediate', true, 'save_intermediates', true);
 
 t_plot = 0:0.01:20;
 %sensitivities_END_plot = sensitivities_function_ENDpiecewise(t_plot);
 sensitivities_END_plot = sensitivities_function_VDE(t_plot);
 
+sensdata_y11 = arrayfun( @(x) x.Gy(1,1), sensitivities_END_plot);
+sensdata_y12 = arrayfun( @(x) x.Gy(1,2), sensitivities_END_plot);
+sensdata_y21 = arrayfun( @(x) x.Gy(2,1), sensitivities_END_plot);
+sensdata_y22 = arrayfun( @(x) x.Gy(2,2), sensitivities_END_plot);
+
 figure(1)
+
 subplot(2,2,1)
-for i = 1:length(t_plot)
-   axis([0 20 0 20])
-   plot(t_plot(i),sensitivities_END_plot(i).Gy(1,1), '.b')
-   hold on
-end
+hold on
+plot(t_plot, sensdata_y11, 'b.')
+axis([0 20 0 20])
 hold off
 xlabel('t');
 ylabel('\partial y_1(t)/\partial y_{0,1}')
@@ -57,24 +61,20 @@ set(gca, 'FontSize', 22);
 set(gca, 'Box', 'off');
 
 subplot(2,2,2)
-for i = 1:length(t_plot)
-   axis([0 20 0 20])
-   plot(t_plot(i),sensitivities_END_plot(i).Gy(1,2), '.b')
-   hold on
-end
+hold on
+plot(t_plot, sensdata_y12, 'b.')
+axis([0 20 0 20])
 hold off
 xlabel('t');
-ylabel('\partial y_1(t)/\partial y_{0,2}')
+ylabel('\partial y_1(t)/\partial y_{0,1}')
 title('G_{y,12}(t; t_0)')
 set(gca, 'FontSize', 22);
 set(gca, 'Box', 'off');
 
 subplot(2,2,3)
-for i = 1:length(t_plot)
-   axis([0 20 0 20])
-   plot(t_plot(i),sensitivities_END_plot(i).Gy(2,1), '.b')
-   hold on
-end
+hold on
+plot(t_plot, sensdata_y21, 'b.')
+axis([0 20 0 20])
 hold off
 xlabel('t');
 ylabel('\partial y_2(t)/\partial y_{0,1}')
@@ -82,18 +82,18 @@ title('G_{y,21}(t; t_0)')
 set(gca, 'FontSize', 22);
 set(gca, 'Box', 'off');
 
+
 subplot(2,2,4)
-for i = 1:length(t_plot)
-   axis([0 20 0 20])
-   plot(t_plot(i),sensitivities_END_plot(i).Gy(2,2), '.b')
-   hold on
-end
+hold on
+plot(t_plot, sensdata_y22, 'b.')
+axis([0 20 0 20])
 hold off
 xlabel('t');
-ylabel('\partial y_2(t)/\partial y_{0,2}')
+ylabel('\partial y_2(t)/\partial y_{0,1}')
 title('G_{y,22}(t; t_0)')
 set(gca, 'FontSize', 22);
 set(gca, 'Box', 'off');
+
 
 %% Plot sensitivities parameters
 
@@ -104,13 +104,15 @@ t_plot = 0:0.01:20;
 %sensitivities_END_plot = sensitivities_function_ENDpiecewise(t_plot);
 sensitivities_END_plot = sensitivities_function_VDE(t_plot);
 
+sensdata_p1 = arrayfun( @(x) x.Gp(1,1), sensitivities_END_plot);
+sensdata_p2 = arrayfun ( @(x) x.Gp(2,1), sensitivities_END_plot);
+
 figure(2)
+
 subplot(1,2,1)
-for i = 1:length(t_plot)
-   axis([0 20 -6 0])
-   plot(t_plot(i),sensitivities_END_plot(i).Gp(1,1), '.b')
-   hold on
-end
+hold on
+plot(t_plot, sensdata_p1, 'b.')
+axis([0 20 -6 0])
 hold off
 xlabel('t');
 ylabel('\partial y_1(t)/\partial p')
@@ -119,11 +121,8 @@ set(gca, 'FontSize', 22);
 set(gca, 'Box', 'off');
 
 subplot(1,2,2)
-for i = 1:length(t_plot)
-   axis([0 20 -6 0])
-   plot(t_plot(i),sensitivities_END_plot(i).Gp(2,1), '.b')
-   hold on
-end
+hold on
+plot(t_plot, sensdata_p1, 'b.')
 hold off
 xlabel('t');
 ylabel('\partial y_2(t)/\partial p')
