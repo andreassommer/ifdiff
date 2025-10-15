5767bc0f82781d9f8aa4a4e360b61070ff45a909
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2025-10-15 10:37:32 +0200

    Adjust path initialization to match new structure

--- toolbox/internal/sensitivities/private/bla.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = bla(inputArg1,inputArg2)
-%BLA Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
