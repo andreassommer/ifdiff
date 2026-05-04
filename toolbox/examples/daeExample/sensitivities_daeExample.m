363337cf6d72ad52bc61c1dc2752cd9ac39add24
pilar <pilar@pilar.domain>
2026-05-04 11:34:41 +0200

    more work on sensitivities for DAEs

--- toolbox/examples/daeExample/sensitivities_daeExample.m
+++ toolbox/examples/daeExample/sensitivities_daeExample.m
@@ -15,20 +15,127 @@ sol = solveODE(datahandle, tspan, x0, p);
 %% Data for finite differences
 dim_y = size(sol.y, 1);
 dim_p = length(p);
-FDstep = generateFDstep(dim_y, dim_p, 'hy_rel_flag', true,'hp_rel_flag', true, 'hy_min', 1e-6, 'hp_min', 1e-6);
+FDstep = generateFDstep(dim_y, dim_p, 'hy_rel_flag', true,'hp_rel_flag', true, 'hy_min', 1e-3, 'hp_min', 1e-3);
 
 %% Generation of sensitivity functions
 tic;
-integrator_options = odeset( 'AbsTol', 1e-14,'RelTol', 1e-12); % generateSensitivityFunction doesn't take mass matrix options
-sensitivities_function_ENDfull = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_full', 'calcGy', true,'calcGp',true, 'Gmatrices_intermediate', true, 'save_intermediates', true);
-sensitivities_function_ENDpiece = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_piecewise', 'calcGy', true,'calcGp',true, 'Gmatrices_intermediate', true, 'save_intermediates', false);
-sensitivities_function_VDE = generateSensitivityFunction(datahandle, sol, FDstep, 'integrator_options', integrator_options, 'method', 'VDE', 'calcGy', true,'calcGp',false, 'Gmatrices_intermediate', true, 'save_intermediates', false);
+integrator_options = odeset('AbsTol', 1e-14,'RelTol', 1e-12, 'MassSingular','no');
+sensitivities_function_ENDfull_DAE = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_full', 'calcGy', true,'calcGp',true, 'Gmatrices_intermediate', true, 'save_intermediates', true);
+sensitivities_function_ENDpiece_DAE = generateSensitivityFunction(datahandle, sol, FDstep, 'method', 'END_piecewise', 'calcGy', true,'calcGp',true, 'Gmatrices_intermediate', true, 'save_intermediates', true);
+sensitivities_function_VDE_DAE = generateSensitivityFunction(datahandle, sol, FDstep, 'integrator_options', integrator_options, 'method', 'VDE', 'calcGy', true,'calcGp',false, 'Gmatrices_intermediate', true, 'save_intermediates', true);
 toc;
 
 %% Sensitivty calculations
-t = [0 3];
+t = [4 5];
 tic;
-sensitivities_full = sensitivities_function_ENDfull(t);
-sensitivities_piece = sensitivities_function_ENDpiece(t);
-sensitivities_VDE = sensitivities_function_VDE(t);
+sensitivities_full_DAE = sensitivities_function_ENDfull_DAE(t);
+sensitivities_piece_DAE = sensitivities_function_ENDpiece_DAE(t);
+sensitivities_VDE_DAE = sensitivities_function_VDE_DAE(t);
 toc;
+
+%% Sensitivities Plots
+
+t_plot_DAE = 0:0.01:5;
+sensitivities_END_plot_DAE = sensitivities_function_ENDfull_DAE(t_plot_DAE); 
+
+sensdata_y11_DAE = arrayfun( @(x) x.Gy(1,1), sensitivities_END_plot_DAE);
+sensdata_y12_DAE = arrayfun( @(x) x.Gy(1,2), sensitivities_END_plot_DAE);
+sensdata_y21_DAE = arrayfun( @(x) x.Gy(2,1), sensitivities_END_plot_DAE);
+sensdata_y22_DAE = arrayfun( @(x) x.Gy(2,2), sensitivities_END_plot_DAE);
+
+
+%{
+figure(1)
+
+subplot(2,2,1)
+hold on
+plot(t_plot_DAE, sensdata_y11_DAE, 'b.')
+axis([0 3 0 3])
+hold off
+xlabel('t');
+ylabel('\partial y_1(t)/\partial y_{0,1}')
+title('G_{y,11}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(2,2,2)
+hold on
+plot(t_plot_DAE, sensdata_y12_DAE, 'b.')
+axis([0 3 0 3])
+hold off
+xlabel('t');
+ylabel('\partial y_1(t)/\partial y_{0,1}')
+title('G_{y,12}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(2,2,3)
+hold on
+plot(t_plot_DAE, sensdata_y21_DAE, 'b.')
+axis([0 3 0 3])
+hold off
+xlabel('t');
+ylabel('\partial y_2(t)/\partial y_{0,1}')
+title('G_{y,21}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+
+subplot(2,2,4)
+hold on
+plot(t_plot_DAE, sensdata_y22_DAE, 'b.')
+axis([0 3 0 3])
+hold off
+xlabel('t');
+ylabel('\partial y_2(t)/\partial y_{0,1}')
+title('G_{y,22}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+figure(1)
+
+subplot(2,2,1)
+hold on
+plot(t_plot_DAE, sensdata_y11_DAE, 'b.')
+axis([0 20 0 20])
+hold off
+xlabel('t');
+ylabel('\partial y_1(t)/\partial y_{0,1}')
+title('G_{y,11}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(2,2,2)
+hold on
+plot(t_plot_DAE, sensdata_y12_DAE, 'b.')
+axis([0 20 0 20])
+hold off
+xlabel('t');
+ylabel('\partial y_1(t)/\partial y_{0,1}')
+title('G_{y,12}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(2,2,3)
+hold on
+plot(t_plot_DAE, sensdata_y21_DAE, 'b.')
+axis([0 20 0 20])
+hold off
+xlabel('t');
+ylabel('\partial y_2(t)/\partial y_{0,1}')
+title('G_{y,21}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+
+subplot(2,2,4)
+hold on
+plot(t_plot_DAE, sensdata_y22_DAE, 'b.')
+axis([0 3 0 3])
+hold off
+xlabel('t');
+ylabel('\partial y_2(t)/\partial y_{0,1}')
+title('G_{y,22}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+%}
