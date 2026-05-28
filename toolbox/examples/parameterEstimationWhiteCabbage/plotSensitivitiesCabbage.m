ed7ba48f827fd26a6a3d7b0f446a6678a157d2b5
pilar <pilar@pilar.domain>
2026-05-28 16:36:25 +0200

    rename in White Cabbage and Predator Prey Filippov

--- toolbox/examples/parameterEstimationWhiteCabbage/plotSensitivitiesCabbage.m
+++ toolbox/examples/parameterEstimationWhiteCabbage/plotSensitivitiesCabbage.m
@@ -1,6 +1,7 @@
 %% Solution ifdiff
 
 integrator = @ode45;
+<<<<<<< HEAD
 odeoptionsrhs_test = odeset( 'AbsTol', 1e-14,'RelTol', 1e-6);
 datahandle    = prepareDatahandleForIntegration('rhsCabbage', 'integrator', integrator, 'options', odeoptionsrhs_test);
 
@@ -8,6 +9,14 @@ datahandle    = prepareDatahandleForIntegration('rhsCabbage', 'integrator', inte
 tspan         = [0 118];
 initialvalues = [2.48252;0;0];
 parameters_ODE = getParamsCabbage();
+=======
+odeoptionsrhs_test = odeset( 'AbsTol', 1e-14,'RelTol', 1e-12);
+datahandle    = prepareDatahandleForIntegration('whiteCabbageRHS', 'integrator', func2str(integrator), 'options', odeoptionsrhs_test);
+
+tspan         = [0 118];
+initialvalues = [2.48252;0;0];
+parameters_ODE = getParams_Cabbage();
+>>>>>>> 60ae785 (rename in White Cabbage and Predator Prey Filippov)
 sol = solveODE(datahandle, tspan, initialvalues, parameters_ODE);
 
 %% Precalculations for finite differences for sensitivities
@@ -25,6 +34,7 @@ sensitivites = sensitivities_function(t_sens);
 
 %%
 figure(1)
+<<<<<<< HEAD
 clf
 
 % Extract Gy matrices
@@ -62,10 +72,129 @@ for row = 1:3
 
     end
 end
+=======
+subplot(3,3,1)
+hold on
+for i = 1:length(t_sens)   
+   plot(t_sens(i),sensitivites(i).Gy(1,1), '.b')
+end
+axis([0 118 0 500])
+hold off
+xlabel('t');
+ylabel('\partial y_L(t)/\partial y_{0,L}')
+title('G_{y,11}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,2)
+ hold on
+for i = 1:length(t_sens)
+   plot(t_sens(i),sensitivites(i).Gy(1,2), '.b')  
+end
+axis([0 118 -1 1.5])
+hold off
+xlabel('t');
+ylabel('\partial y_L(t)/\partial y_{0,S}')
+title('G_{y,12}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,3)
+hold on
+for i = 1:length(t_sens)   
+   plot(t_sens(i),sensitivites(i).Gy(1,3), '.b')
+end
+axis([0 118 -1 1.5])
+hold off
+xlabel('t');
+ylabel('\partial y_L(t)/\partial y_{0,H}')
+title('G_{y,13}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,4)
+hold on
+for i = 1:length(t_sens)   
+   plot(t_sens(i),sensitivites(i).Gy(2,1), '.b')   
+end
+axis([0 118 0 500])
+hold off
+xlabel('t');
+ylabel('\partial y_S(t)/\partial y_{0,L}')
+title('G_{y,21}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,5)
+ hold on
+for i = 1:length(t_sens)   
+   plot(t_sens(i),sensitivites(i).Gy(2,2), '.b')  
+end
+axis([0 118 -1 1.5])
+hold off
+xlabel('t');
+ylabel('\partial y_S(t)/\partial y_{0,S}')
+title('G_{y,22}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,6)
+hold on
+for i = 1:length(t_sens)   
+   plot(t_sens(i),sensitivites(i).Gy(2,3), '.b')   
+end
+axis([0 118 -1 1.5])
+hold off
+xlabel('t');
+ylabel('\partial y_S(t)/\partial y_{0,H}')
+title('G_{y,23}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,7)
+hold on
+for i = 1:length(t_sens)   
+   plot(t_sens(i),sensitivites(i).Gy(3,1), '.b')   
+end
+axis([0 118 0 500])
+hold off
+xlabel('t');
+ylabel('\partial y_H(t)/\partial y_{0,L}')
+title('G_{y,31}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,8)
+hold on
+for i = 1:length(t_sens)   
+   plot(t_sens(i),sensitivites(i).Gy(3,2), '.b')   
+end
+axis([0 118 -1 1.5])
+hold off
+xlabel('t');
+ylabel('\partial y_H(t)/\partial y_{0,S}')
+title('G_{y,32}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,9)
+hold on
+for i = 1:length(t_sens)   
+   plot(t_sens(i),sensitivites(i).Gy(3,3), '.b')   
+end
+axis([0 118 -1 1.5])
+hold off
+xlabel('t');
+ylabel('\partial y_H(t)/\partial y_{0,H}')
+title('G_{y,33}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+>>>>>>> 60ae785 (rename in White Cabbage and Predator Prey Filippov)
 
 %% Plot sensitivities parameters
 sensitivities_function_p = generateSensitivityFunction(datahandle, sol, 'FDstep', FDstep, 'calcGy', false, 'method', method, 'p_typ', parameters_ODE);
 t_sens = 0:0.1:118;
+<<<<<<< HEAD
 ticid = tic;
 sensitivites = sensitivities_function_p(t_sens);
 toc(ticid);
@@ -113,3 +242,125 @@ for row = 1:3
         set(gca, 'GridAlpha', 0.15)
     end
 end
+=======
+sensitivites = sensitivities_function_p(t_sens);
+
+%%
+figure(2)
+subplot(3,3,1)
+for i = 1:length(t_sens)
+   axis([0 118 0 0.02])
+   plot(t_sens(i),sensitivites(i).Gp(1,1), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_L(t)/\partial a')
+title('G_{p,11}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,2)
+for i = 1:length(t_sens)
+   axis([0 118 -20 150])
+   plot(t_sens(i),sensitivites(i).Gp(1,5), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_L(t)/\partial r_S')
+title('G_{p,15}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,3)
+for i = 1:length(t_sens)
+   axis([0 118 0 4000])
+   plot(t_sens(i),sensitivites(i).Gp(1,6), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_L(t)/\partial r_H')
+title('G_{p,16}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,4)
+for i = 1:length(t_sens)
+   axis([0 118 0 0.02])
+   plot(t_sens(i),sensitivites(i).Gp(2,1), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_S(t)/\partial a')
+title('G_{p,21}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,5)
+for i = 1:length(t_sens)
+   axis([0 118 -20 150])
+   plot(t_sens(i),sensitivites(i).Gp(2,5), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_S(t)/\partial r_S')
+title('G_{p,25}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,6)
+for i = 1:length(t_sens)
+   axis([0 118 0 4000])
+   plot(t_sens(i),sensitivites(i).Gp(2,6), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_S(t)/\partial r_H')
+title('G_{p,26}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,7)
+for i = 1:length(t_sens)
+   axis([0 118 0 0.02])
+   plot(t_sens(i),sensitivites(i).Gp(3,1), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_H(t)/\partial a')
+title('G_{p,31}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,8)
+for i = 1:length(t_sens)
+   axis([0 118 -20 150])
+   plot(t_sens(i),sensitivites(i).Gp(3,5), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_H(t)/\partial r_S')
+title('G_{p,35}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+
+subplot(3,3,9)
+for i = 1:length(t_sens)
+   axis([0 118 0 4000])
+   plot(t_sens(i),sensitivites(i).Gp(3,6), '.b')
+   hold on
+end
+hold off
+xlabel('t');
+ylabel('\partial y_H(t)/\partial r_H')
+title('G_{p,36}(t; t_0)')
+set(gca, 'FontSize', 22);
+set(gca, 'Box', 'off');
+>>>>>>> 60ae785 (rename in White Cabbage and Predator Prey Filippov)
