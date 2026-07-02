27b70bc9d926fa3342b4a13f9073f8cf909c7340
pilar <pilar@pilar.domain>
2026-07-02 19:16:40 +0200

    Renamed Task 1, added Task3 (unfinished)

---
+++ toolbox/solutions/rhsTask3.m
@@ -0,0 +1,16 @@
+function f = rhsTask3(~, x, p)
+
+    f = zeros(2,1);
+
+    % algebraic constraint
+    z = x(1) + x(2) + x(2)^3 - p; 
+    f(2) = z;
+
+    % differential equation
+    if x(2) < p
+        f(1) = x(2);
+    else
+        f(1) = 0;
+    end
+
+end
