1ba3545498a6422f1ab66e82d6a405e4cc0829f9
Lev Gromov <lev.gromov@stud.uni-heidelberg.de>
2026-08-23 16:05:51 +0200

    Document derivative classes

--- toolbox/internal/sensitivities/IFDIFFDerivative.m
+++ toolbox/internal/sensitivities/IFDIFFDerivative.m
@@ -1,4 +1,16 @@
 classdef IFDIFFDerivative < handle
+    %IFDIFFDERIVATIVE - Interface for derivatives used by IFDIFF
+    %
+    %    Description
+    %      Declares methods for computing directional derivatives of functions (in particular RHS, submodel, switching and jump functions)
+    %      which return some quantity given a time point, state vector and parameter vector.
+    %
+    %    Methods
+    %      dt/dy/dp - Compute directional derivative w.r.t. t/y/p for an arbitrary number of directions
+    %
+    %    See also IFDIFFDERIVATIVEFINITEDIFFERENCES
+
+
     methods (Abstract, Access=public)
         d = dt(this, t, y, p, v)
         d = dy(this, t, y, p, v)
