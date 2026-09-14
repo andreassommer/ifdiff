0d707ad0f4a10c4641fdbccb3a96e3d7c6c1b1f5
pilar <pilar@pilar.domain>
2026-09-14 11:52:08 +0200

    minor running time related changes in computeCI.m

--- toolbox/examples/parameterEstimationWhiteCabbage/HelpersWhiteCabbage/computeCI.m
+++ toolbox/examples/parameterEstimationWhiteCabbage/HelpersWhiteCabbage/computeCI.m
@@ -8,22 +8,26 @@ np = numel(param);
 nr = numel(res);
 dof = nr - np;
 
-% variance
-s2 = (transpose(res) * res) / dof;
+% Approximation when a column is zero vector
+temp = find(max(abs(J)) == 0);
+if ~isempty(temp)
+    J(:,temp) = sqrt(eps(class(J)));
+end
 
 % QR factorization for J and computation of
 % parameter covariance matrix
 [~,R] = qr(J,0);
 Rinv = R \ eye(size(R));
-CovP = s2 * (Rinv * transpose(Rinv));
+diag_info = sum(Rinv.*Rinv, 2);
+rmse = norm(res)/sqrt(dof);
 
 % critical t-value
-% we use 1-alpha=0.95 => alpha=0.05
-% and hence tcrit=1-alpha/2 = 0.975
+% we use alpha = 0.05
+% and hence tcrit = 1-alpha/2 = 0.975
 tcrit = tInv(0.975, dof);
 
 % standard error
-SE = sqrt(diag(CovP));
+SE = sqrt(diag_info)*rmse;
 
 CI = [param - tcrit*SE, param + tcrit*SE];
 
